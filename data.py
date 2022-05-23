8f7e359b9c530d105d569f5edeb1b99043b364e2
SAMRIDHISINHA <samridhisinha1234@gmail.com>
2022-05-23 23:25:54 +0530

    Linked images function for database and recognition

---
+++ data.py
@@ -0,0 +1,26 @@
+import os
+import cv2
+
+path = 'images'
+images = []
+personName = []
+myList = os.listdir(path)
+try:
+    myList.remove('.DS_Store')
+except:
+    pass
+# print(myList)
+for cur_img in myList:
+    current_img = cv2.imread(f'{path}/{cur_img}')
+    images.append(current_img)
+    personName.append(os.path.splitext(cur_img)[0])
+print(personName)
+
+def image_indir():
+    return images
+def image_name():
+    return personName
+
+
+
+
