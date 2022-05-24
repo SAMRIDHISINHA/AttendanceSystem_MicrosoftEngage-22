421a1a35218095931d47dcc467936dee8115a3ab
SAMRIDHISINHA <samridhisinha1234@gmail.com>
2022-05-24 11:00:14 +0530

    Added login credentials

---
+++ StudentData.py
@@ -0,0 +1,28 @@
+dict = {}
+
+
+dict['Samridhi'] = "samridhi"
+dict['Angelina'] = "angelina"
+dict['Biden'] = "biden"
+dict['Emma'] = "emma"
+dict['Harry'] = "harry"
+dict['Jeff'] = "jeff"
+dict['Messi'] = "messi"
+dict['Nick'] = "nick"
+dict['Obama'] = "obama"
+dict['Parag'] = "parag"
+dict['Satya'] = "satya"
+dict['Kevin'] = "kevin"
+dict['Peeyush'] = "peeyush"
+dict['Messi'] = "messi"
+dict['Ronaldo'] = "ronaldo"
+dict['Taylor'] = "Taylor"
+dict['Zayn'] = "zayn"
+dict['Trump'] = "trump"
+dict['Bhuvan'] = "bhuvan"
+dict['Sundar'] = "sundar"
+
+
+
+def auth_users():
+    return dict
---
+++ TeacherData.py
@@ -0,0 +1,9 @@
+dict = {}
+
+dict['Admin'] = "complexno1"
+dict['Maths'] = "algebra"
+
+
+def auth_admin():
+    return dict
+    
