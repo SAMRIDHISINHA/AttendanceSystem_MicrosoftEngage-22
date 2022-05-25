7276c70b6d921605920c0511762baa100da7235a
SAMRIDHISINHA <samridhisinha1234@gmail.com>
2022-05-25 16:45:02 +0530

    Added emotion detector,drowsiness detector and major bugs fix

--- StudentData.py
+++ StudentData.py
@@ -2,25 +2,11 @@ dict = {}
 
 
 dict['Samridhi'] = "samridhi"
-dict['Angelina'] = "angelina"
 dict['Biden'] = "biden"
-dict['Emma'] = "emma"
-dict['Harry'] = "harry"
-dict['Jeff'] = "jeff"
-dict['Messi'] = "messi"
-dict['Nick'] = "nick"
-dict['Obama'] = "obama"
-dict['Parag'] = "parag"
 dict['Satya'] = "satya"
-dict['Kevin'] = "kevin"
-dict['Peeyush'] = "peeyush"
-dict['Messi'] = "messi"
-dict['Ronaldo'] = "ronaldo"
 dict['Taylor'] = "Taylor"
-dict['Zayn'] = "zayn"
-dict['Trump'] = "trump"
-dict['Bhuvan'] = "bhuvan"
-dict['Sundar'] = "sundar"
+dict['Narendra'] = "narendra"
+
 
 
 
